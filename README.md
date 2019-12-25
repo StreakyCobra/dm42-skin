ea21d3ca9dafe0a7a3828f185b49b1cbc7e97623
Fabien Dubosson <fabien.dubosson@gmail.com>
2019-12-25 18:06:55 +0100

    Initial version of dm42-skin

---
+++ README.md
@@ -0,0 +1,67 @@
+# DM42 skin for the Free42 simulator
+
+This repository contains the sources for generating a [DM42][] skin for the
+[Free42 simulator][].
+
+## Quick install
+
+1. Download the desired skin from the [releases page][] (see Notes below
+   regarding the sizes), and extract the files form the `ZIP`.
+
+2. Copy the extracted `.gif` and `.layout` files to the [Free42 skin folder][].
+
+3. Select the newly installed skin in the Free42 simulator.
+
+
+## Building from source
+
+1. Install python `pillow` dependency:
+
+        pip install pillow
+
+2. Run the `generate.py` script with the desired parameters (see Notes below
+   regarding size):
+
+        ./generate.py 3
+
+3. Copy the generated `.gif` and `.layout` files from `skins/<NAME>/` to the
+   [Free42 skin folder][.
+
+4. Select the newly installed skin in the Free42 simulator.
+
+## Notes
+
+**TL;DR**: _You probably want size 2 or 3 on your Laptop if the size of the
+simulator is fixed by the size of the skin. On your phone where the simulator
+adapt to the size of the screen, you probably want the best image quality, go
+for 12._
+
+The GTK version of Free42 for Linux can not — as far as I know — be scaled
+manually, that means the size of the `GIF` defines the size of the simulator on
+screen. People have personal preferences regarding the on-screen size of
+their calculator, so I made a script to generate different desired skin sizes.
+
+It is not always possible to use a floating-value display scales
+([source](https://thomasokken.com/free42/skins/#nonint)):
+
+> Non-integer display scales are supported in Free42 for Android, iOS, and
+> MacOS, release 2.0.24g and later.
+
+That means the display can only be a multiple of — the original — 131 pixels
+wide. And if we want the labels on screen to be aligned with the buttons bellow,
+the skin can only be scaled up and down by integer values as well.
+
+The solution I made is the `generate.py` scripts that take 3 parameters:
+
+    ./generate.py [<x_magnification>] [<y_magnification>] [<skin_name>]
+
+The `x` magnification constrains and defines the final size of the skin. The `y`
+magnification can be used to have taller (or smaller) digits on screen, but it
+does not have impact on the skin size. If omitted if defaults to twice the `x`
+magnification. Finally the skin name can be optionally given, allowing you to
+give meaningful names to your skins.
+
+[releases page]: https://github.com/StreakyCobra/dm42-skin/releases
+[DM42]: https://www.swissmicros.com/dm42.php
+[Free42 simulator]: https://thomasokken.com/free42/
+[Free42 skin folder]: https://thomasokken.com/free42/skins/README.html
---
+++ generate.py
@@ -0,0 +1,230 @@
+#!/usr/bin/env python3
+"""Generate DM42 skin configuration files for Free42."""
+
+import json
+import os
+import sys
+from functools import partial
+
+from PIL import Image
+
+# Original size in pixels of the HP42
+ORIG_HP42_WIDTH = 131
+ORIG_HP42_HEIGHT = 16
+
+# File containing the layout data
+LAYOUT_FILENAME = "dm42.json"
+
+# The skin of the DM42
+SKIN_FILENAME = "dm42.png"
+SKIN_WIDTH = 2903
+SKIN_HEIGHT = 5462
+SKIN_ACTIVE_SHIFT = 5700
+
+# The size, position and colors of the display in the original GIF
+DISPLAY_WIDTH = 2206
+DISPLAY_LEFT = 350
+DISPLAY_BOTTOM = 1713
+DISPLAY_FOREGROUND = "2c302e"
+DISPLAY_BACKGROUND = "caccc9"
+
+
+# --------------------------------------------------------------------------- #
+# CREATE SKIN FILES                                                           #
+# --------------------------------------------------------------------------- #
+
+
+def resize_image(mx, my, filename):
+    """Resize the skin image to fit the given x and y magnification."""
+    coef = compute_coef(mx, my)
+    img = Image.open(SKIN_FILENAME)
+    size = [scale(v, coef) for v in img.size]
+    img.resize(size).save(filename, "GIF", optimize=True)
+
+
+def generate_layout(mx, my, filename):
+    """Generate the layout file with given x and y magnification."""
+    # Compute the scaling coefficient
+    coef = compute_coef(mx, my)
+    # Load the layout data
+    with open(LAYOUT_FILENAME, "r") as f:
+        data = json.load(f)
+    # Generate the layout file
+    with open(filename, "w") as f:
+        write = partial(print, file=f)
+        write(gen_headers())
+        write()
+        write(gen_skin(coef))
+        write(gen_display(mx, my, coef))
+        write()
+        write(gen_keys(data, coef))
+        write()
+        write(gen_macros(data))
+        write()
+        write(gen_annunciators(data, coef))
+        write()
+
+
+# --------------------------------------------------------------------------- #
+# GENERATE LAYOUT PARTS                                                       #
+# --------------------------------------------------------------------------- #
+
+
+def gen_headers():
+    """Generate the headers of the layout file."""
+    lines = [
+        "# DM42 skin for the Free42 simulator",
+        "https://github.com/StreakyCobra/dm42-skin"
+        "By Fabien Dubosson <fabien.dubosson@gmail.com>",
+    ]
+    return "\n".join(lines)
+
+
+def gen_skin(coef):
+    """Generate the «Skin» line of the layout file."""
+    skin = [scale_str(v, coef) for v in [0, 0, SKIN_WIDTH, SKIN_HEIGHT]]
+    return f"Skin: {','.join(skin)}"
+
+
+def gen_display(mx, my, coef):
+    """Generate the «Display» line of the layout file."""
+    left = scale(DISPLAY_LEFT, coef)
+    top = round(DISPLAY_BOTTOM * coef - ORIG_HP42_HEIGHT * my)
+    return (
+        f"Display: {left},{top} {mx} {my} " f"{DISPLAY_BACKGROUND} {DISPLAY_FOREGROUND}"
+    )
+
+
+def gen_keys(data, coef):
+    """Generate the «Key» lines of the layout file."""
+    keys = data.get("keys", {})
+    keys = {i: expend_key_data(v) for i, v in keys.items()}
+    keys = {i: scale_str(v, coef) for i, v in keys.items()}
+    keys = [
+        f"Key: {i} "
+        + ",".join(k["sensitivity"])
+        + " "
+        + ",".join(k["display"])
+        + " "
+        + ",".join(k["active"])
+        for i, k in keys.items()
+    ]
+    return "\n".join(keys)
+
+
+def gen_macros(data):
+    """Generate the «Macro» lines of the layout file."""
+    macros = data.get("macros", {})
+    macros = [f"Macro: {i} " + " ".join(as_str(m)) for i, m in macros.items()]
+    return "\n".join(macros)
+
+
+def gen_annunciators(data, coef):
+    """Generate the «Annunciator» lines of the layout file."""
+    annunciators = data.get("annunciators", {})
+    annunciators = {i: scale_str(v, coef) for i, v in annunciators.items()}
+    annunciators = [
+        f"Annunciator: {i} " + ",".join(k["rectangle"]) + " " + ",".join(k["active"])
+        for i, k in annunciators.items()
+    ]
+    return "\n".join(annunciators)
+
+
+# --------------------------------------------------------------------------- #
+# HELPERS                                                                     #
+# --------------------------------------------------------------------------- #
+
+
+def scale(obj, coef, as_str=False):
+    """Scale an object recursively with the given coefficient."""
+    f = str if as_str else int
+    if hasattr(obj, "items"):
+        return {k: scale(v, coef, as_str) for k, v in obj.items()}
+    try:
+        return [scale(v, coef, as_str) for v in obj]
+    except TypeError:
+        return f(round(obj * coef))
+
+
+def scale_str(obj, coef):
+    """Scale a object with the given coefficient and return it as str."""
+    return scale(obj, coef, as_str=True)
+
+
+def as_str(obj):
+    """Return an object as string recursively."""
+    if hasattr(obj, "items"):
+        return {k: as_str(v) for k, v in obj.items()}
+    try:
+        return [as_str(v) for v in obj]
+    except TypeError:
+        return str(obj)
+
+
+def compute_coef(mx, my):
+    """Compute the scaling coefficient from the given x and y magnification."""
+    return (mx * ORIG_HP42_WIDTH) / DISPLAY_WIDTH
+
+
+def expend_key_data(key):
+    """Expend key data in case of missing information."""
+    if "display" not in key:
+        key["display"] = key["sensitivity"]
+    if "active" not in key:
+        key["active"] = [
+            key["sensitivity"][0],
+            SKIN_ACTIVE_SHIFT + key["sensitivity"][1],
+        ]
+    return key
+
+
+# --------------------------------------------------------------------------- #
+# RUNNING SCRIPT                                                              #
+# --------------------------------------------------------------------------- #
+
+
+def main():
+    """Run the script."""
+
+    # Handle help flag
+    if "-h" in sys.argv:
+        usage(sys.argv[0])
+
+    # Get arguments
+    if len(sys.argv) > 1:
+        mx = int(sys.argv[1])  # x magnification
+    else:
+        mx = 1
+    if len(sys.argv) > 2:
+        my = int(sys.argv[2])  # y magnification
+    else:
+        my = mx * 2
+    if len(sys.argv) > 3:
+        name = sys.argv[3]  # Name of the skin
+    else:
+        name = f"dm42_{mx}"
+
+    # Ensure the output directory exits
+    folder = os.path.join("skins", name)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    # Resize the skin image
+    resize_image(mx, my, os.path.join(folder, f"{name}.gif"))
+
+    # Generate the associated skin layout
+    generate_layout(mx, my, os.path.join(folder, f"{name}.layout"))
+
+
+def usage(program):
+    print("Usage:\n")
+    print(f"\t{program} [<x_magnification>] [<y_magnification>] [<skin_name>]\n")
+    print(f"Examples:\n")
+    print(f"\t{program} 2")
+    print(f"\t{program} 2 5")
+    print(f"\t{program} 2 5 'dm42-custom'")
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
